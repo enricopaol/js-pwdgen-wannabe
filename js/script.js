58560e1af98e605d0577305ce1aadad187de8f98
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-03-18 14:47:47 +0100

    Funzionamento base

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-var ciaoSonoVar = 15;
-console.log(ciaoSonoVar);
+//Chiedi all’utente il suo nome, poi chiedi il suo cognome, 
+//poi chiedi il suo colore preferito Infine scrivi sulla pagina 
+//nomecognomecolorepreferito21
+
+// Variabili
+var nomeUtente = prompt ('Qual è il tuo nome?');
+var cognomeUtente = prompt ('Qual è il tuo cognome?');
+var colorePreferito = prompt ('Qual è il tuo colore preferito?');
+var currentYear = 21;
+var password
+
+//console.log(nomeUtente);
+//console.log(cognomeUtente);
+//console.log(colorePreferito);
+
+// Logica
+password = nomeUtente + cognomeUtente + colorePreferito + currentYear;
+
+document.getElementById('password').innerHTML = password;
